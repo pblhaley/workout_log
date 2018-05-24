2b847de86692c96dd59b355a916ec53830374de7
Peter Haley <pblhaley@gmail.com>
2018-05-23 22:12:07 -0500

    add app gems and workout form/views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,5 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -36,6 +37,7 @@ class WorkoutsController < ApplicationController
   end
 
   def find_workout
+    @workout = Workout.find(params[:id])
   end
 
 end
